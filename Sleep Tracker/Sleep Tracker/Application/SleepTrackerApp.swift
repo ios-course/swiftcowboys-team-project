9b9d3d5c4638fe7668179dd9c9c969af5fd79c97
Nik_Gribok <mushroomicus@gmail.com>
2023-05-06 12:12:35 +0400

    Issue#1 Create app project with environment and base arcitecture folders

---
+++ Sleep Tracker/Sleep Tracker/Application/SleepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sleep_TrackerApp.swift
+//  Sleep Tracker
+//
+//  Created by Kolya Gribok on 6.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SleepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

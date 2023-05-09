0cc70444f49ccd3f3d6706275354c7af39d578af
Yegor <97677516+meschya@users.noreply.github.com>
2023-05-09 18:38:27 +0300

    reset `ContentView`

--- Sleep Tracker/Sleep Tracker/View/ContentView.swift
+++ Sleep Tracker/Sleep Tracker/View/ContentView.swift
@@ -3,7 +3,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            SleepSetupView()
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
         }
         .padding()
     }

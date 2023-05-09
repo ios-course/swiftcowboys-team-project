bebf19bc235a3ea273158130a4df3620fbf5003d
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-05-09 20:54:01 +0200

    add property `alarmStatusText`

--- Sleep Tracker/Sleep Tracker/View/DuringSleepView/DuringSleepView.swift
+++ Sleep Tracker/Sleep Tracker/View/DuringSleepView/DuringSleepView.swift
@@ -1,14 +1,19 @@
 import SwiftUI
 
 struct DuringSleepView: View {
+
+    // MARK: - Public interface
     var body: some View {
         VStack {
             Image("Sleeping_astronaut_image")
-            Text("Alarm 07:30")
+            Text(alarmStatusText)
                 .font(.system(size: 25))
                 .foregroundColor(.white)
         }
     }
+
+    // MARK: - Private interface
+    @State private var alarmStatusText = "Alarm 07:30"
 }
 
 struct DuringSleepView_Previews: PreviewProvider {

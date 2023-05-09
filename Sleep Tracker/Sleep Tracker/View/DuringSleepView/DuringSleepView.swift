79f1d2ef14396fe2b37063899437c0d1b68c1fc3
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-05-09 20:34:25 +0200

    create folder `DuringSleepView` and `SwiftUI` file `DuringSleepView`

---
+++ Sleep Tracker/Sleep Tracker/View/DuringSleepView/DuringSleepView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct DuringSleepView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DuringSleepView_Previews: PreviewProvider {
+    static var previews: some View {
+        DuringSleepView()
+    }
+}

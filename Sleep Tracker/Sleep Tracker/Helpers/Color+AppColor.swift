2e45b84140a963b9e5459a53620a057d0f2448eb
marinkman <marinkman@gmail.com>
2023-05-09 20:27:45 +0300

    change color format

--- Sleep Tracker/Sleep Tracker/Helpers/Color+AppColor.swift
+++ Sleep Tracker/Sleep Tracker/Helpers/Color+AppColor.swift
@@ -4,24 +4,24 @@ extension Color {
     /// The color representation used in the application that adapts to the given context.
     enum AppColor {
         /// A dark indigo color suitable for use in UI elements.
-        static let darkIndigo = Color(red: 0.145, green: 0.09, blue: 0.318)
+        static let darkIndigo = Color(red: 37.0 / 255.0, green: 23.0 / 255.0, blue: 81.0 / 255.0)
         /// A bright blue color suitable for use in UI elements.
-        static let brightBlue = Color(red: 0.22, green: 0.157, blue: 0.718)
+        static let brightBlue = Color(red: 56.0 / 255.0, green: 40.0 / 255.0, blue: 183.0 / 255.0)
         /// A blue purple color suitable for use in UI elements.
-        static let bluePurple = Color(red: 0.616, green: 0.569, blue: 1)
+        static let bluePurple = Color(red: 157.0 / 255.0, green: 145.0 / 255.0, blue: 255.0 / 255.0)
         /// A blue violet color suitable for use in UI elements.
-        static let blueViolet = Color(red: 0.184, green: 0.145, blue: 0.439)
+        static let blueViolet = Color(red: 47.0 / 255.0, green: 37.0 / 255.0, blue: 122.0 / 255.0)
         /// A bright purple color suitable for use in UI elements.
-        static let brightPurple = Color(red: 0.192, green: 0.106, blue: 0.42)
+        static let brightPurple = Color(red: 49.0 / 255.0, green: 27.0 / 255.0, blue: 107.0 / 255.0)
         /// A bright mint color suitable for use in UI elements.
-        static let brightMint = Color(red: 0.569, green: 1, blue: 0.715)
+        static let brightMint = Color(red: 145.0 / 255.0, green: 255.0 / 255.0, blue: 182.0 / 255.0)
         /// A dark graphite color suitable for use in UI elements.
-        static let darkGraphite = Color(red: 0.094, green: 0.114, blue: 0.169)
+        static let darkGraphite = Color(red: 24.0 / 255.0, green: 29.0 / 255.0, blue: 43.0 / 255.0)
         /// A light gray color suitable for use in UI elements.
-        static let lightGray = Color(red: 0.769, green: 0.769, blue: 0.769)
+        static let lightGray = Color(red: 196.0 / 255.0, green: 196.0 / 255.0, blue: 196.0 / 255.0)
         /// A white color suitable for use in UI elements.
-        static let white = Color(red: 1, green: 1, blue: 1)
+        static let white = Color(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0)
         /// A black color suitable for use in UI elements.
-        static let black = Color(red: 0, green: 0, blue: 0)
+        static let black = Color(red: 0.0 / 255.0, green: 0.0 / 255.0, blue: 0.0 / 255.0)
     }
 }

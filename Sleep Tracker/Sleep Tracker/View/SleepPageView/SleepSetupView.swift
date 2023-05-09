9397d9670f8ee8560db8f316ded67765d651aa3c
Yegor <97677516+meschya@users.noreply.github.com>
2023-05-09 15:50:51 +0300

    create `SleepPageView` folder + `SleepSetupView` SwiftUIView file

---
+++ Sleep Tracker/Sleep Tracker/View/SleepPageView/SleepSetupView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SleepSetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SleepSetupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SleepSetupView()
+    }
+}

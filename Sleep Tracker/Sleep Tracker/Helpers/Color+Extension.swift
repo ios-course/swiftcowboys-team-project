211b841c86f4f15807d2f6106ccde12a4b1c2d27
marinkman <marinkman@gmail.com>
2023-05-09 10:49:04 +0300

    add documentation to the `Custom` enum

--- Sleep Tracker/Sleep Tracker/Helpers/Color+Extension.swift
+++ Sleep Tracker/Sleep Tracker/Helpers/Color+Extension.swift
@@ -1,27 +1,27 @@
 import SwiftUI
 
 extension Color {
-    // A custom color representation that adapts to the given context.
+    /// A custom color representation that adapts to the given context.
     enum Custom {
-        // A dark indigo color suitable for use in UI elements.
+        /// A dark indigo color suitable for use in UI elements.
         static let darkIndigo = Color(red: 0.145, green: 0.09, blue: 0.318)
-        // A bright blue color suitable for use in UI elements.
+        /// A bright blue color suitable for use in UI elements.
         static let brightBlue = Color(red: 0.22, green: 0.157, blue: 0.718)
-        // A blue purple color suitable for use in UI elements.
+        /// A blue purple color suitable for use in UI elements.
         static let bluePurple = Color(red: 0.616, green: 0.569, blue: 1)
-        // A blue violet color suitable for use in UI elements.
+        /// A blue violet color suitable for use in UI elements.
         static let blueViolet = Color(red: 0.184, green: 0.145, blue: 0.439)
-        // A bright purple color suitable for use in UI elements.
+        /// A bright purple color suitable for use in UI elements.
         static let brightPurple = Color(red: 0.192, green: 0.106, blue: 0.42)
-        // A bright mint color suitable for use in UI elements.
+        /// A bright mint color suitable for use in UI elements.
         static let brightMint = Color(red: 0.569, green: 1, blue: 0.715)
-        // A dark graphite color suitable for use in UI elements.
+        /// A dark graphite color suitable for use in UI elements.
         static let darkGraphite = Color(red: 0.094, green: 0.114, blue: 0.169)
-        // A light gray color suitable for use in UI elements.
+        /// A light gray color suitable for use in UI elements.
         static let lightGray = Color(red: 0.769, green: 0.769, blue: 0.769)
-        // A white color suitable for use in UI elements.
+        /// A white color suitable for use in UI elements.
         static let white = Color(red: 1, green: 1, blue: 1)
-        // A black color suitable for use in UI elements.
+        /// A black color suitable for use in UI elements.
         static let black = Color(red: 0, green: 0, blue: 0)
     }
 }

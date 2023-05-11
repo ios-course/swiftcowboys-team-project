e09895bfe66bd715f2faa9b872e8de04b344d0e8
marinkman <marinkman@gmail.com>
2023-05-11 18:55:59 +0300

    create `AppImage` enum

--- Sleep Tracker/Sleep Tracker/Helpers/Image+AppImage.swift
+++ Sleep Tracker/Sleep Tracker/Helpers/Image+AppImage.swift
@@ -1,5 +1,20 @@
 import SwiftUI
 
 extension Image {
-    
+    enum AppImage {
+        static let alarmSymbolWhite = Image("Alarm_symbol_white")
+        static let bedSymbolWhite = Image("Bed_symbol_white")
+        static let exitSymbolBlack = Image("Exit_symbol_black@3x")
+        static let generalBackgroundImage = Image("General_background_image")
+        static let homeSymbolPurple = Image("Home_symbol_purple")
+        static let moonSymbolPurple = Image("Moon_symbol_purple")
+        static let moonWithStarsSymbolColored = Image("Moon_with_stars_symbol_colored")
+        static let moonWithStarsSymbolPurple = Image("Moon_with_stars_symbol_purple")
+        static let onboardingBackgroundImage = Image("Onboarding_background_image")
+        static let sleepingAstronautImage = Image("Sleeping_astronaut_image")
+        static let statisticsSymbolPurple = Image("Statistics_symbol_purple")
+        static let sunSymbolColored = Image("Sun_symbol_colored")
+        static let tickMarkSymbolWhite = Image("Tick-mark_symbol_white")
+        static let viewSymbolPurple = Image("View_symbol_purple")
+    }
 }

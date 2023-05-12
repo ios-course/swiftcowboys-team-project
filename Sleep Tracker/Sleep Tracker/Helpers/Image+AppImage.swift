3195c1a342939bd704b184479227f94ee82d582c
marinkman <marinkman@gmail.com>
2023-05-12 20:10:50 +0300

    rename some images and image files

--- Sleep Tracker/Sleep Tracker/Helpers/Image+AppImage.swift
+++ Sleep Tracker/Sleep Tracker/Helpers/Image+AppImage.swift
@@ -10,7 +10,7 @@ extension Image {
         /// An image of a black exit symbol.
         static let exitSymbolBlack = Image("Exit_symbol_black")
         /// A general background image.
-        static let generalBackgroundImage = Image("General_background_image")
+        static let generalBackground = Image("General_background")
         /// An image of a purple home symbol.
         static let homeSymbolPurple = Image("Home_symbol_purple")
         /// An image of a purple moon symbol.
@@ -20,9 +20,9 @@ extension Image {
         /// An image of a purple moon symbol with stars.
         static let moonWithStarsSymbolPurple = Image("Moon_with_stars_symbol_purple")
         /// An onboarding background image.
-        static let onboardingBackgroundImage = Image("Onboarding_background_image")
+        static let onboardingBackground = Image("Onboarding_background")
         /// A sleeping astronaut image.
-        static let sleepingAstronautImage = Image("Sleeping_astronaut_image")
+        static let sleepingAstronaut = Image("Sleeping_astronaut")
         /// An image of a statistic symbol.
         static let statisticsSymbolPurple = Image("Statistics_symbol_purple")
         /// An image of a colored sun symbol.

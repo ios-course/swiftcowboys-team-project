990c755496eb274e115e6d719bed553ac1f32524
marinkman <marinkman@gmail.com>
2023-05-12 16:56:08 +0300

    correct the documentation

--- Sleep Tracker/Sleep Tracker/Helpers/Image+AppImage.swift
+++ Sleep Tracker/Sleep Tracker/Helpers/Image+AppImage.swift
@@ -3,33 +3,33 @@ import SwiftUI
 extension Image {
     /// The image used in the application.
     enum AppImage {
-        /// An image of a white alarm symbol suitable for use in UI.
+        /// An image of a white alarm symbol.
         static let alarmSymbolWhite = Image("Alarm_symbol_white")
-        /// An image of a white bed symbol suitable for use in UI.
+        /// An image of a white bed symbol.
         static let bedSymbolWhite = Image("Bed_symbol_white")
-        /// An image of a black exit symbol suitable for use in UI.
+        /// An image of a black exit symbol.
         static let exitSymbolBlack = Image("Exit_symbol_black@3x")
-        /// A general background image suitable for use in UI.
+        /// A general background image.
         static let generalBackgroundImage = Image("General_background_image")
-        /// An image of a purple home symbol suitable for use in UI.
+        /// An image of a purple home symbol.
         static let homeSymbolPurple = Image("Home_symbol_purple")
-        /// An image of a purple moon symbol suitable for use in UI.
+        /// An image of a purple moon symbol.
         static let moonSymbolPurple = Image("Moon_symbol_purple")
-        /// An image of a colored moon symbol with stars suitable for use in UI.
+        /// An image of a colored moon symbol with stars.
         static let moonWithStarsSymbolColored = Image("Moon_with_stars_symbol_colored")
-        /// An image of a purple moon symbol with stars suitable for use in UI.
+        /// An image of a purple moon symbol with stars.
         static let moonWithStarsSymbolPurple = Image("Moon_with_stars_symbol_purple")
-        /// An onboarding background image suitable for use in UI.
+        /// An onboarding background image.
         static let onboardingBackgroundImage = Image("Onboarding_background_image")
-        /// A sleeping astronaut image suitable for use in UI.
+        /// A sleeping astronaut image.
         static let sleepingAstronautImage = Image("Sleeping_astronaut_image")
-        /// An image of a statistic symbol suitable for use in UI.
+        /// An image of a statistic symbol.
         static let statisticsSymbolPurple = Image("Statistics_symbol_purple")
-        /// An image of a colored sun symbol suitable for use in UI.
+        /// An image of a colored sun symbol.
         static let sunSymbolColored = Image("Sun_symbol_colored")
-        /// An image of a white symbol with a tick mark suitable for use in UI.
+        /// An image of a white symbol with a tick mark.
         static let tickMarkSymbolWhite = Image("Tick-mark_symbol_white")
-        /// An image of a purple view symbol suitable for use in UI.
+        /// An image of a purple view symbol.
         static let viewSymbolPurple = Image("View_symbol_purple")
     }
 }

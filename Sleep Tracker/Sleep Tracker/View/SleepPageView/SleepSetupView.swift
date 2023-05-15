251c48b00130b932b58c77f7de441b1c3dc86556
Yegor <97677516+meschya@users.noreply.github.com>
2023-05-16 00:10:17 +0300

    correcting comments in the code + add documentation + create `noAlarmLabel` and logic hide or show

--- Sleep Tracker/Sleep Tracker/View/SleepPageView/SleepSetupView.swift
+++ Sleep Tracker/Sleep Tracker/View/SleepPageView/SleepSetupView.swift
@@ -1,34 +1,53 @@
 import SwiftUI
 
+/**
+ A view showing the option to set the alarm clock and set the wake-up time before going to bed.
+ */
 struct SleepSetupView: View {
-    @State var animate = false
+    // MARK: - Public interface
+
+    /**
+     Boolean value by which we can know if the alarm is on or off
+     */
+    @State var isAlarmOn = false
+
+    // MARK: - Private interface
+
     @State private var currentDate = Date()
 
     var body: some View {
         VStack {
-            Image("Sleeping_astronaut_image")
-                .resizable()
-                .frame(width: 150, height: 150)
-
+            sleepingAstronautImage
             HStack {
                 alarmButton
                     .onTapGesture {
                         withAnimation(Animation.spring().speed(1)) {
-                            animate.toggle()
+                            isAlarmOn.toggle()
                         }
                     }
-                    .zIndex(1)
-                timePicker
-                    .padding(.leading, 40)
-                    .zIndex(0)
+                ZStack {
+                    timePicker
+                    noAlarmLabel
+                }
             }
             estimatedLabel
-                .padding(.top, 50)
         }
     }
 
+    /**
+     Creating an image with an astronaut.
+     */
+    private var sleepingAstronautImage: some View {
+        Image.AppImage.sleepingAstronaut
+            .resizable()
+            .frame(width: 150, height: 150)
+    }
+
+    /**
+     Creating a custom alarm button when pressed, changes the value of `isAlarmOn` to the opposite.
+     */
     private var alarmButton: some View {
-        Image("Alarm_symbol_white")
+        Image.AppImage.alarmSymbolWhite
             .resizable()
             .frame(width: 20, height: 20)
             .background {
@@ -36,20 +55,40 @@ struct SleepSetupView: View {
                     .fill(.purple)
                     .frame(width: 70, height: 40)
             }
-            .opacity(animate ? 1.0 : 0.5)
+            .opacity(isAlarmOn ? 1.0 : 0.5)
+            .zIndex(1)
     }
 
+    /**
+     Creating a custom TimePicker in which the user will be able to choose what time he wakes up.
+     */
     private var timePicker: some View {
         DatePicker("", selection: $currentDate, displayedComponents: .hourAndMinute)
             .labelsHidden()
             .datePickerStyle(.wheel)
             .frame(width: 200, height: 100)
             .clipped()
+            .padding(.leading, 40)
+            .zIndex(0)
+            .opacity(isAlarmOn ? 1.0 : 0.0)
     }
 
+    /**
+     Creating a label if the alarm clock is not on.
+     */
+    private var noAlarmLabel: some View {
+        Text("No alarm,\nonly sleep analized")
+            .font(.system(size: 20, weight: .bold))
+            .opacity(isAlarmOn ? 0.0 : 1.0)
+    }
+
+    /**
+     Creating a custom Estimated Label which shows how long the user will sleep.
+     */
     private var estimatedLabel: some View {
         Text("estimated bedtime 7:52")
             .font(.system(size: 20, weight: .bold))
+            .padding(.top, 50)
     }
 }
 

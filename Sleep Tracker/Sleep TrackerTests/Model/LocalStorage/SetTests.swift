2c62cd1c9ae6c286a156fa0f18f5dcd16f089522
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-05-16 23:23:36 +0200

    implement tests for `set()` method

---
+++ Sleep Tracker/Sleep TrackerTests/Model/LocalStorage/SetTests.swift
@@ -0,0 +1,47 @@
+import XCTest
+
+final class SetTests: XCTestCase {
+    override func setUp() {
+        storage = LocalStorage()
+    }
+
+    func testSetPresentsInTheAPI() {
+        // given
+        storage.set("value", forKey: "key")
+
+        // when
+
+        // then
+        // the code compiles
+    }
+
+    func testSetReturnsNilForNonExistentKey() {
+        // given
+        let numberToSet = 100
+        let key = "number"
+
+        // when
+        storage.set(numberToSet, forKey: key)
+        let retrievedValue: Int? = storage.get(forKey: key)
+
+        // then
+        XCTAssertEqual(numberToSet, retrievedValue)
+    }
+
+    func testSetSetsTheValueForExistentKey() {
+        // given
+        let englishGreeting = "Hello"
+        let spanishGreeting = "Hola"
+        let key = "greeting"
+
+        // when
+        storage.set(englishGreeting, forKey: key)
+        storage.set(spanishGreeting, forKey: key)
+        let retrievedValue: String? = storage.get(forKey: key)
+
+        // then
+        XCTAssertEqual(spanishGreeting, retrievedValue)
+    }
+
+    private var storage = LocalStorage()
+}

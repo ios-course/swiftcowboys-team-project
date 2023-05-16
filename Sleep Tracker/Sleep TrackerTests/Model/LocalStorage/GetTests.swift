d8100f54b8be04d3ea805416ec764e11bc32830f
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-05-16 23:24:02 +0200

    implement tests for `get()` method

---
+++ Sleep Tracker/Sleep TrackerTests/Model/LocalStorage/GetTests.swift
@@ -0,0 +1,56 @@
+import XCTest
+
+final class GetTests: XCTestCase {
+    override func setUp() {
+        storage = LocalStorage()
+    }
+
+    func testGetPresentsInTheAPI() {
+        // given
+        let _: Bool? = storage.get(forKey: "key")
+
+        // when
+
+        // then
+        // the code compiles
+    }
+
+    func testGetReturnsTheValueForExistingkey() {
+        // given
+        let numberToSet = 100
+        let key = "number"
+
+        // when
+        storage.set(numberToSet, forKey: key)
+        let retrievedValue: Int? = storage.get(forKey: key)
+
+        // then
+        XCTAssertEqual(numberToSet, retrievedValue)
+    }
+
+    func testGetReturnsNilForNonExistentKey() {
+        // given
+        let nonExistentKey = "nonExistentKey"
+
+        // when
+        let retrievedValue: String? = storage.get(forKey: nonExistentKey)
+
+        // then
+        XCTAssertNil(retrievedValue)
+    }
+
+    func testGetReturnsNilWhenThereIsAValueOfDifferentTypeForExistentKey() {
+        // given
+        let key = "isAlarmOn"
+        let alarmStatus = "true"
+
+        // when
+        storage.set(alarmStatus, forKey: key)
+        let retrievedValue: Bool? = storage.get(forKey: key)
+
+        // then
+        XCTAssertNil(retrievedValue)
+    }
+
+    private var storage = LocalStorage()
+}

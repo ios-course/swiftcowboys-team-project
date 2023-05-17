44a97532ceef93c65aa871efb212fb22dea9d426
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-05-17 21:02:06 +0200

    improved the documentation

--- Sleep Tracker/Sleep Tracker/Model/LocalStorage.swift
+++ Sleep Tracker/Sleep Tracker/Model/LocalStorage.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-/// A wrapper that provides a convenient interface to interact with local storage using `UserDefaults`.
+/// A local storage that can store key-value pairs persistently across launches of the app.
 final class LocalStorage {
     // MARK: - Public interface
 
-    /// Sets the given value for the given key in local storage.
+    /// Stores the given value for the given key in a local storage.
     /// If a value for the key already exists in the storage, it is overridden.
-    ///
+    /// - Note: The method is thread-safe.
     /// - Parameters:
     ///   - value: The value to be stored.
     ///   - key: The key associated with the value.
@@ -14,17 +14,17 @@ final class LocalStorage {
         defaults.set(value, forKey: key)
     }
 
-    /// Retrieves the value associated with the given key from local storage.
-    ///
+    /// Retrieves the value of specified type, associated with the given key from local storage.
+    /// - Note: It attempts to cast the value to the specified type, returning the value if the cast is successful.
     /// - Parameter key: The key associated with the value.
     /// - Returns: The value associated with the key, or `nil` if the key is not found
-    /// or the value has a different type.
+    /// or the value has a type different from the specified one.
     func get<T>(forKey key: String) -> T? {
         return defaults.object(forKey: key) as? T
     }
 
     /// Deletes the value associated with the given key from local storage.
-    ///
+    /// If no value is found for the given key, the method has no effect.
     /// - Parameter key: The key associated with the value.
     func delete(forKey key: String) {
         defaults.removeObject(forKey: key)

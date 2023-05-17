5bd7dd33105b8fcbc12d282b8bb28c31c776681e
Mikhail Kosarev <mikhailk.developer@gmail.com>
2023-05-17 22:31:51 +0200

    refactor `LocalStorageDeleteTests`

--- Sleep Tracker/Sleep TrackerTests/Model/LocalStorage/DeleteTests.swift
+++ Sleep Tracker/Sleep TrackerTests/Model/LocalStorage/DeleteTests.swift
@@ -6,59 +6,59 @@ final class LocalStorageDeleteTests: XCTestCase {
     }
 
     func testDeletePresentsInTheAPI() {
-        // given
-        storage.delete(forKey: "key")
-
-        // when
+        // given // when
+        storage.delete(forKey: "vt78hwc789hr43-key")
 
         // then
         // the code compiles
     }
 
-    func testDeleteRemovesValueForExistingkey() {
+    func testDeleteRemovesValueForExistingKey() {
         // given
-        let numberToSet = 100
-        let key = "number"
-        storage.set(numberToSet, forKey: key)
+        let testValue = 100
+        let key = "c1y4gns7h7h3q9-numberKey"
+        storage.set(testValue, forKey: key)
 
         // when
         storage.delete(forKey: key)
 
-        let retrievedValue: Int? = storage.get(forKey: key)
-
         // then
+        let retrievedValue: Int? = storage.get(forKey: key)
         XCTAssertNil(retrievedValue)
     }
 
-    func testDeleteRemovesValueForNonExistingkey() {
+    func testDeleteRemovesValueForNonExistingKey() {
         // given
-        let nonExistentKey = "nonExistentKey"
+        let nonExistentKey = "sdjiofsjio-nonExistentKey"
 
         // when
         storage.delete(forKey: nonExistentKey)
-        let retrievedValue: String? = storage.get(forKey: nonExistentKey)
 
         // then
+        let retrievedValue: String? = storage.get(forKey: nonExistentKey)
         XCTAssertNil(retrievedValue)
     }
 
-    func testDeleteRemovesCorrespondingValuesWhenDeletesForAFewKeys() {
+    func testDeleteRemovesOnlyCorrespondingValuesWhenDeletesForAFewKeys() {
         // Given
-        storage.set("Value 1", forKey: "key1")
-        storage.set("Value 2", forKey: "key2")
-        storage.set("Value 3", forKey: "key3")
+        let key1 = "23rh89ciuwa-key1"
+        let key2 = "chn4rnuinef-key2"
+        let key3 = "3fouiednsnn-key3"
+        storage.set("Value 1", forKey: key1)
+        storage.set("Value 2", forKey: key2)
+        storage.set("Value 3", forKey: key3)
 
         // When
-        storage.delete(forKey: "key1")
-        storage.delete(forKey: "key3")
-
-        let value1: String? = storage.get(forKey: "key1")
-        let value3: String? = storage.get(forKey: "key3")
+        storage.delete(forKey: key1)
+        storage.delete(forKey: key3)
 
         // Then
+        let value1: String? = storage.get(forKey: key1)
+        let value3: String? = storage.get(forKey: key3)
+
         XCTAssertNil(value1)
         XCTAssertNil(value3)
-        XCTAssertEqual(storage.get(forKey: "key2"), "Value 2")
+        XCTAssertEqual(storage.get(forKey: key2), "Value 2")
     }
 
     private var storage = LocalStorage()

5e54ae3ef189b79296fc61ece78ae62fa9581ffc
marinkman <marinkman@gmail.com>
2023-05-19 17:19:48 +0300

    create `AppUIConstant` enum

---
+++ Sleep Tracker/SleepTracker/Helpers/AppUIConstant.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+/// The size of the UI element used in the application.
+enum AppUIConstant {
+    /// A corner radius is 10.
+    static let cornerRadius10: CGFloat = 10
+    /// A corner radius is 46.
+    static let cornerRadius46: CGFloat = 46
+    /// A corner radius is 50.
+    static let cornerRadius50: CGFloat = 50
+    /// A corner radius is 15.
+    static let cornerRadius15: CGFloat = 15
+    /// A corner radius is 80.
+    static let cornerRadius80: CGFloat = 80
+
+    /// A padding is 24.
+    static let padding24: CGFloat = 24
+    /// A padding is 50.
+    static let padding50: CGFloat = 50
+    /// A padding is 10.
+    static let padding10: CGFloat = 10
+    /// A padding is 25.
+    static let padding25: CGFloat = 25
+
+    /// An opacity is 0.0.
+    static let transparent: Double = 0.0
+    /// An opacity is 0.5.
+    static let translucent: Double = 0.5
+    /// An opacity is 1.0.
+    static let opaque: Double = 1.0
+
+    /// A frame width is 0.
+    static let frameWidth0: CGFloat = 0
+    /// A frame height is 100.
+    static let frameHeight100: CGFloat = 100
+
+    /// An animation speed is 1.0.
+    static let animationSpeed1: Double = 1.0
+}

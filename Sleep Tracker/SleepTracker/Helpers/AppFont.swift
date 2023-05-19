4315f36a9bcb2a11ad896b567981f78689c91f89
marinkman <marinkman@gmail.com>
2023-05-19 17:17:21 +0300

    create `AppFont` enum

---
+++ Sleep Tracker/SleepTracker/Helpers/AppFont.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+/// The font used in the application.
+enum AppFont {
+    /// A font size is 10.
+    static let size10: CGFloat = 10
+    /// A font size is 12.
+    static let size12: CGFloat = 12
+    /// A font size is 16.
+    static let size16: CGFloat = 16
+    /// A font size is 18.
+    static let size18: CGFloat = 18
+    /// A font size is 20.
+    static let size20: CGFloat = 20
+    /// A font size is 20.
+    static let size25: CGFloat = 25
+    /// A font size is 40.
+    static let size40: CGFloat = 40
+    /// A font size is 48.
+    static let size48: CGFloat = 48
+    /// A font size is 64.
+    static let size64: CGFloat = 64
+}

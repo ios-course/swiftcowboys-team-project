9a24906a1fa2f78c6ee9ab5805c7249f15c09ea7
marinkman <marinkman@gmail.com>
2023-05-19 21:37:18 +0300

    correct documentation

--- Sleep Tracker/SleepTracker/Helpers/AppFont.swift
+++ Sleep Tracker/SleepTracker/Helpers/AppFont.swift
@@ -12,7 +12,7 @@ enum AppFont {
     static let size18: CGFloat = 18
     /// A font size is 20.
     static let size20: CGFloat = 20
-    /// A font size is 20.
+    /// A font size is 25.
     static let size25: CGFloat = 25
     /// A font size is 40.
     static let size40: CGFloat = 40

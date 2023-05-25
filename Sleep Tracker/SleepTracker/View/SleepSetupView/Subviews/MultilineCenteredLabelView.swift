6e44e8837c324178e66fc6e25c10a911f1ccd37a
Dersim Davaod <dersim-davaod@Dersims-MacBook-Pro.local>
2023-05-25 10:09:24 +0300

    Rename EstimatedSleepDurationLabelView to MultilineCenteredLabelView.

---
+++ Sleep Tracker/SleepTracker/View/SleepSetupView/Subviews/MultilineCenteredLabelView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+/// A label that shows a center-aligned multiline bold text.
+struct MultilineCenteredLabelView: View {
+    let text: String
+
+    var body: some View {
+        Text(text)
+            .multilineTextAlignment(.center)
+            .font(
+                .system(
+                    size: 20,
+                    weight: .bold
+                )
+            )
+    }
+}
+
+struct MultilineCenteredLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        MultilineCenteredLabelView(
+            text: "The first line text\nThe second line text"
+        )
+    }
+}

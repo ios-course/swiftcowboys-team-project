5a2f17b5b229418dbc51ce348b4464f2a8fafe83
marinkman <marinkman@gmail.com>
2023-05-26 12:07:45 +0300

    update `AppUIConstant` enum

--- Sleep Tracker/SleepTracker/Helpers/AppUIConstant.swift
+++ Sleep Tracker/SleepTracker/Helpers/AppUIConstant.swift
@@ -1,34 +1,25 @@
 import SwiftUI
 
-/// The size of the UI element used in the application.
+/// The setting of the UI element used in the application.
 enum AppUIConstant {
-    /// A corner radius is 10.
-    static let cornerRadius10: CGFloat = 10
-    /// A corner radius is 46.
-    static let cornerRadius46: CGFloat = 46
-    /// A corner radius is 20.
-    static let cornerRadius20: CGFloat = 20
-    /// A corner radius is 15.
-    static let cornerRadius15: CGFloat = 15
-    /// A corner radius is 80.
-    static let cornerRadius80: CGFloat = 80
+    /// A base corner radius.
+    static let baseCornerRadius: CGFloat = 10
+    /// A corner radius used for rounded alarm view.
+    static let alarmCornerRadius: CGFloat = 20
+    /// A corner radius used for tab bar.
+    static let tabBarCornerRadius: CGFloat = 50
+    /// A corner radius used for large elements.
+    static let largeCornerRadius: CGFloat = 80
 
-    /// A padding is 24.
-    static let padding24: CGFloat = 24
-    /// A padding is 50.
-    static let padding50: CGFloat = 50
-    /// A padding is 10.
-    static let padding10: CGFloat = 10
-
-    /// An opacity is 0.0.
+    /// An opacity setting used for the transparent view.
     static let transparent: Double = 0.0
-    /// An opacity is 0.5.
+    /// An opacity setting used for the translucent view.
     static let translucent: Double = 0.5
-    /// An opacity is 1.0.
+    /// An opacity setting used for the opaque view.
     static let opaque: Double = 1.0
 
-    /// A frame size is 0.
-    static let frameSize0: CGFloat = 0
-    /// A frame size is 100.
-    static let frameSize100: CGFloat = 100
+    /// A minimum frame width.
+    static let minFrameWidth: CGFloat = 0
+    /// A maximum frame height.
+    static let maxFrameHeight: CGFloat = 100
 }
--- Sleep Tracker/SleepTrackerUITests/Helpers/AppUIConstantTests.swift
+++ Sleep Tracker/SleepTrackerUITests/Helpers/AppUIConstantTests.swift
@@ -1,39 +1,29 @@
 import XCTest
 
 final class AppUIConstantTests: XCTestCase {
-    func testAppUIConstantSizesReturnCorrectValues() {
-        let cornerRadius10: CGFloat = 10
-        let cornerRadius46: CGFloat = 46
-        let cornerRadius20: CGFloat = 20
-        let cornerRadius15: CGFloat = 15
-        let cornerRadius80: CGFloat = 80
-
-        let padding24: CGFloat = 24
-        let padding50: CGFloat = 50
-        let padding10: CGFloat = 10
+    func testAppUIConstantReturnsCorrectValues() {
+        let baseCornerRadius: CGFloat = 10
+        let alarmCornerRadius: CGFloat = 20
+        let tabBarCornerRadius: CGFloat = 50
+        let largeCornerRadius: CGFloat = 80
 
         let transparent = 0.0
         let translucent = 0.5
         let opaque = 1.0
 
-        let frameSize0: CGFloat = 0
-        let frameSize100: CGFloat = 100
+        let minFrameWidth: CGFloat = 0
+        let maxFrameHeight: CGFloat = 100
 
-        XCTAssertEqual(cornerRadius10, AppUIConstant.cornerRadius10)
-        XCTAssertEqual(cornerRadius46, AppUIConstant.cornerRadius46)
-        XCTAssertEqual(cornerRadius20, AppUIConstant.cornerRadius20)
-        XCTAssertEqual(cornerRadius15, AppUIConstant.cornerRadius15)
-        XCTAssertEqual(cornerRadius80, AppUIConstant.cornerRadius80)
-
-        XCTAssertEqual(padding24, AppUIConstant.padding24)
-        XCTAssertEqual(padding50, AppUIConstant.padding50)
-        XCTAssertEqual(padding10, AppUIConstant.padding10)
+        XCTAssertEqual(baseCornerRadius, AppUIConstant.baseCornerRadius)
+        XCTAssertEqual(alarmCornerRadius, AppUIConstant.alarmCornerRadius)
+        XCTAssertEqual(tabBarCornerRadius, AppUIConstant.tabBarCornerRadius)
+        XCTAssertEqual(largeCornerRadius, AppUIConstant.largeCornerRadius)
 
         XCTAssertEqual(transparent, AppUIConstant.transparent)
         XCTAssertEqual(translucent, AppUIConstant.translucent)
         XCTAssertEqual(opaque, AppUIConstant.opaque)
 
-        XCTAssertEqual(frameSize0, AppUIConstant.frameSize0)
-        XCTAssertEqual(frameSize100, AppUIConstant.frameSize100)
+        XCTAssertEqual(minFrameWidth, AppUIConstant.minFrameWidth)
+        XCTAssertEqual(maxFrameHeight, AppUIConstant.maxFrameHeight)
     }
 }

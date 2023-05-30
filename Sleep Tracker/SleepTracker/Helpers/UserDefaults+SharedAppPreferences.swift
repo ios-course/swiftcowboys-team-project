a4d11e46a4e8f383b94db9dff591709ea16a2ccd
Dersim Davaod <dersim-davaod@Dersims-MacBook-Pro.local>
2023-05-30 09:38:27 +0300

    Implement UserDefaults+SharedAppPreferences extension.

---
+++ Sleep Tracker/SleepTracker/Helpers/UserDefaults+SharedAppPreferences.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+extension UserDefaults {
+    /// A global instance of `UserDefaults` configured to store the app-specific preferences.
+    static var sharedAppPreferences: UserDefaults? {
+        let bundleIdPrefix = Bundle.main.bundleIdentifier ?? "ios-app"
+        let storagePrefix = "key-value-storage-over-the-ios-user-defaults-storage"
+        let storageName = "app-preferences"
+
+        return UserDefaults(suiteName: "\(bundleIdPrefix)-\(storagePrefix)-\(storageName)")
+    }
+
+    /// Keys to use within the app when accessing `UserDefauts`.
+    enum Key {
+        /// References the state of the sleep.
+        static let sleepingState = "sleepingState"
+    }
+}

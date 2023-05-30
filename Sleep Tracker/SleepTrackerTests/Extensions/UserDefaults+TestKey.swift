1794e28a248ffa9d42c0eeb3eb54daf3be0b6bed
Dersim Davaod <dersim-davaod@Dersims-MacBook-Pro.local>
2023-05-30 14:28:03 +0300

    Fix format issues.

--- Sleep Tracker/SleepTrackerTests/Extensions/UserDefaults+TestKey.swift
+++ Sleep Tracker/SleepTrackerTests/Extensions/UserDefaults+TestKey.swift
@@ -3,7 +3,6 @@ import Foundation
 extension UserDefaults {
     /// Keys in the key-value storage to use in unit tests.
     enum TestKey: String, CaseIterable {
-
         /// An example of key to use in the key-value storage.
         case keyA = "unit-test-keyA"
     }

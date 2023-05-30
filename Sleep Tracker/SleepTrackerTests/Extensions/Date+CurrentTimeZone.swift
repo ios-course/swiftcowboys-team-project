654f6ca0957720a593329b93912cc78f867c5fa9
Dersim Davaod <dersim-davaod@Dersims-MacBook-Pro.local>
2023-05-30 17:43:50 +0300

    Add missing docs.

--- Sleep Tracker/SleepTrackerTests/Extensions/Date+CurrentTimeZone.swift
+++ Sleep Tracker/SleepTrackerTests/Extensions/Date+CurrentTimeZone.swift
@@ -1,6 +1,7 @@
 import Foundation.NSDate
 
 extension Date {
+    /// Date converted to the time zone currently used by the system.
     var toCurrentTimeZone: Date {
         addingTimeInterval(
             TimeInterval(

34a63131c38c7fdffb520eab5d1d0d8f26ee0656
Yegor <97677516+meschya@users.noreply.github.com>
2023-06-06 11:59:13 +0300

    create `RoundedRectangleView` + `ReportDetailsView`

---
+++ SleepTracker/SleepTracker/View/SleepReportPageView/ReportDetailsView/ReportDetailsView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ReportDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReportDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReportDetailsView()
+    }
+}
